2904a4a7c1cb6567a34343088df2d9f34d8e0bfd
Quema <105044971+Quema100@users.noreply.github.com>
2022-08-10 14:14:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ history.pushState(null, null, window.location.href);
         history.go(1);
 };
 window.addEventListener('load', function(){
-    if(window.location.href.startsWith('https://www.youtube.com/channel/UCfbvz7xPaOOxiAYqRhycQ2g'))return;
-    var link =window.location.replace('https://www.youtube.com/channel/UCfbvz7xPaOOxiAYqRhycQ2g');
+    if(window.location.href.startsWith('URL'))return;
+    var link =window.location.replace('URL');
     if(window.location.href !== link){ 
         while(link===link){     
        
